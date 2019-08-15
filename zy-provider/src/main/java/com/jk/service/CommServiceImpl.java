a54331f03171f6ee85d0b6fca3debcfb02a7ebee
ZY2822692153 <2822692153@qq.com>
2019-08-15 20:29:28 +0800

    消费者上传

--- zy-provider/src/main/java/com/jk/service/CommServiceImpl.java
+++ zy-provider/src/main/java/com/jk/service/CommServiceImpl.java
@@ -1,6 +1,22 @@
 package com.jk.service;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.PageHelper;
+import com.jk.dao.CommDao;
+import com.jk.util.PageUtil;
+import com.jk.util.ParameUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Service(version = "1.0")
+@Component
 public class CommServiceImpl implements CommService {
+@Autowired
+    private CommDao da;
 
-
+    @Override
+    public PageUtil cha(ParameUtil param) {
+        PageHelper.startPage(param.getPageNumber(),param.getPageSize());
+        return null;
+    }
 }
