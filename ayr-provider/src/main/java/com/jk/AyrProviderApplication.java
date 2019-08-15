6e1ad56fdb0df1348872d56b4f048818fb4415a8
wzh123 <weizhenhua1997@qq.com>
2019-08-15 15:26:54 +0800

    未振华第一次上传

---
+++ ayr-provider/src/main/java/com/jk/AyrProviderApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyrProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyrProviderApplication.class, args);
+    }
+
+}
---
+++ mqc-provider/src/main/java/com/jk/MqcProviderApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqcProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqcProviderApplication.class, args);
+    }
+
+}
