3d63e74871d66f5d7e7849a5d9e80c3f18e8adfe
zsg0604 <1005227857@qq.com>
2019-08-15 17:46:02 +0800

    张松光第2次上传

---
+++ api_car/src/main/java/com/jk/model/Userrole.java
@@ -0,0 +1,44 @@
+package com.jk.model;
+
+import java.io.Serializable;
+
+/**
+ * @author ： 张松光
+ * @date ：Created in 2019/8/15 9:59
+ * @description：员工权限表
+ * @package ：com.jk.model
+ * @version: 1.0.0
+ */
+public class Userrole implements Serializable {
+
+    private Integer id;
+
+    private Integer uid;
+
+    private Integer rid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+
+}
