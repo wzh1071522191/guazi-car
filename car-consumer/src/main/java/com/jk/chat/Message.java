5089338c4ef04e3ee08730be0f6286d7d70d1a2c
ZY2822692153 <2822692153@qq.com>
2019-08-26 19:05:48 +0800

    上传

--- car-consumer/src/main/java/com/jk/chat/Message.java
+++ car-consumer/src/main/java/com/jk/chat/Message.java
@@ -18,13 +18,20 @@ public class Message {
     public static final String SPEAK = "SPEAK";
     public static final String QUIT = "QUIT";
 
-    private String type;//消息类型
+    public String type;//消息类型
 
-    private String username; //发送人
+    public String username; //发送人
 
-    private String msg; //发送消息
+    public String msg; //发送消息
 
-    private int onlineCount; //在线用户数
+    public int onlineCount; //在线用户数
+
+    public Message(String type, String username, String msg, int onlineCount) {
+        this.type = type;
+        this.username = username;
+        this.msg = msg;
+        this.onlineCount = onlineCount;
+    }
 
     public static String jsonStr(String type, String username, String msg, int onlineTotal) {
         return JSON.toJSONString(new Message(type, username, msg, onlineTotal));
